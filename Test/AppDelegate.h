433179d10f3e671a09d853f3c62a913b8d93088a
ICK Training <icktraining_01@ICKs-Mac-mini.local>
2013-04-05 18:52:52 +0200

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by ICK Training on 4/5/13.
+//  Copyright (c) 2013 ICK Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by ICK Training on 4/5/13.
+//  Copyright (c) 2013 ICK Training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
